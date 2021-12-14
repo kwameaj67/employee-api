dda76d3762b8bd5d6f4375661a9f8a73f2766aee
kwame boateng <kwame.agyenim-boateng@petraonline.com>
2021-12-14 15:07:29 +0000

    created service interface

---
+++ RestAPI_Demo/Services/IEmployeeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestAPI_Demo.Models
+{
+     public interface IEmployeeService
+    {
+        Task<List<EmployeeModel>> getEmployees();
+        Task<EmployeeModel> getEmployeeByName(string name);
+        Task<EmployeeModel> getEmployee(int id);
+        Task<EmployeeModel> createEmployee(EmployeeModel employee);
+        Task<EmployeeModel> deleteEmployee(int id);
+        Task<EmployeeModel> editEmployee(int id,EmployeeModel employee);
+
+        
+    }
+
+}
